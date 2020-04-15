037e735a4d9e70fe4968701dc09fd973d3474e29
spdin <saripudin@konvergen.ai>
2020-04-15 21:29:50 +0700

    delete credentials information

--- python/config.py
+++ python/config.py
@@ -1,12 +1,12 @@
 # API based account of @saripudin_gon
 
-consumer_key = 'ukzzwYITH4Tkg3R9qvZ9HvDS6'
-consumer_secret = 'lmJawNSoI3OyibOybIVH4RIEffPRpusJvjnPQep2D7WZcehBZ7'
-access_token = '94953993-xQUVZRv3vHY0IntT1fm6aqcmDCB5HJ1Go3IHYBNtl'
-access_secret = 'wOUA4JiYXf2NepEHnZmwsPN0aNJHeTZesZKqATSD0VDPK'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_secret = ''
 
-host = 'localhost'
-user = 'root'
-passwd = 'khaddimHuzur'
-database = 'twitter_streaming'
+host = ''
+user = ''
+passwd = ''
+database = ''
 
